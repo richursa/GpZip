4b603005402252f71e3b9a71e40643d384b9010b
richursa <richursa@gmail.com>
2019-01-12 17:29:24 +0530

    cpu working

--- burrowsWheelerTransform.cpp
+++ burrowsWheelerTransform.cpp
@@ -1,37 +1,24 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
-int main()
+void swap(int *pointer1 , int *pointer2)
 {
-    char *stringArray = new char[1024];
-    cout<<"enter the string ";
-    cin>>stringArray;
-    char *pointerArray[1024]; //pointer to the different rotations of string
-    for(int i=0;i<1024;i++)
-    {
-        pointerArray[i] = stringArray[i];
-    }
-
+    int temp;
+    temp = *pointer1;
+    *pointer1 = *pointer2;
+    *pointer2 = temp;
 }
-void stringBubbleSort(char *pointerArray , int size)
+int stringCompare(char string1[] , char string2[], int size)
 {
-    for(int  i =0 ;i<size;i++)
-    {
-        for(j=i;j<size-1;j++)
-        {
-            if(stringCompare(pointerArray[i],pointerArray[i+1])
-        }
-    }
-}
-int stringCompare(char *string1 , char *string2, int length)
-{
+    int flag=0;
     for(int i=0; i<size;i++)
-    {
-        if(string1[i]>string2[i))
+    { 
+        if(string1[i]>string2[i])
         {
             flag = 1;
             break;
         }
-        else if(string1[i]<string2[i]))
+        else if(string1[i]<string2[i])
         {
             flag = -1;
             break;
@@ -42,4 +29,34 @@ int stringCompare(char *string1 , char *string2, int length)
         }
     }
     return flag;
+}
+void stringBubbleSort(char stringArray[],int pointerArray[], int size)
+{
+    for(int  i =0 ;i<size;i++)
+    {
+        for(int j=0;j<size-1;j++)
+        {
+            if(stringCompare(&stringArray[0]+pointerArray[j],&stringArray[0]+pointerArray[j]+1,size)==1)
+            {
+                swap(pointerArray+j,pointerArray+j+1);
+            }
+        }
+    }
+}
+int main()
+{
+    char stringArray[1024];
+    cout<<"enter the string";
+    cin>>stringArray;
+    int size = strlen(stringArray);
+    int pointerArray[size];
+    for(int i=0;i<size;i++)
+    {
+        pointerArray[i] = i;
+    }
+    stringBubbleSort(stringArray,pointerArray,size);
+    for(int i =0 ;i<size;i++)
+    {
+        cout<<stringArray[pointerArray[i]+size-1];
+    }
 }
